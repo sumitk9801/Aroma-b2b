304dffe24139f3a38f19da5d754dd13a523fe075
Sumit <Sumitkhandelwal547@gmail.com>
2026-05-27 00:00:58 +0530

    role auth been setup

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -18,5 +18,13 @@ const auth = async (req, res, next) => {
         res.status(401).json({ message: "Invalid token" });
     }
 };
+const authorize = (roles) => {
+    return (req, res, next) => {
+        if ("customer"== req.user.role) {
+            return res.status(403).json({ message: "Forbidden: You don't have permission to access this resource" });
+        }
+        next();
+    };
+};
 
 module.exports = auth;
